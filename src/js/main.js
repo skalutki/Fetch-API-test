7061530ec50176e10b728043caecfef754682c44
skalutki <skalutki@gmail.com>
2020-04-24 22:09:47 +0100

    feat:first leyout

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,30 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
 
 // place your code below
 
+const list = document.querySelector('.list--js');
 
-console.log(`Hello world!`);
-
-
+fetch("https://api.github.com/users/skalutki/repos?sort=updated&direction=asc")
+    .then(resp => resp.json())
+    .then(resp => {
+        const repos = resp;
+        for (const repo of repos) {
+            console.log(repo.name)
+            list.innerHTML += `<li class="list__element"> <a href = "${repo.html_url}"> ${repo.name}</a> </li>`;
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
